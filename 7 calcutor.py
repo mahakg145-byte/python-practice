3825855084eb8151bd89ba82f323bc3156396a6a
Mahak Gupta <mahakg145@gmail.com>
2026-08-16 18:09:16 +0530

    Add calculator program

---
+++ 7 calcutor.py
@@ -0,0 +1,15 @@
+'''print(5+6)   # addition
+print(35-5)   # Subtration
+print(15*6)    # multiplication
+print(15/6)     #division
+print(5%3)      #modulus
+print(15//4)    #floor division
+print(5**3)      #exponential''' 
+ 
+a = int(input("Enter a 1st number: "))
+b = int(input("Enter 2nd nummber:  "))
+print("adition of", a,"+",b,"is:", a+ b)
+print("subtraction of a-b is",a-b)
+print("multiplication of a*b is",int(a)*int(b))
+print("division of a/b is",int(a)/int(b))
+print("modulus of a%b is",int(a)%int(b))
